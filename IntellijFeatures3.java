ae1bcb05d9eb43cc2a7ade2cb5131568598e227e
Eren <mrdagdemir@gmail.com>
2022-07-05 23:29:05 -0400

    class17

---
+++ IntellijFeatures3.java
@@ -0,0 +1,14 @@
+package com.syntax.class17;
+
+public class IntellijFeatures3 {
+
+    public static void main(String[] args) {
+        int [] arr={10,20,30,40,50};
+
+        for (int j : arr) {
+            System.out.println(j);
+        }
+
+    }
+}
+
